d7f4efec6933b2aa24a625cac582ead8331eb40d
oznur.demir <dmroznurr@gmail.com>
2024-08-23 18:25:17 +0300

    Completed detay_sayfa.dart UI.

--- lib/ui/views/detay_sayfa.dart
+++ lib/ui/views/detay_sayfa.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:user_app/data/entity/kisiler.dart';
 
 class DetaySayfa extends StatefulWidget {
-  const DetaySayfa({super.key});
+  // Bu sayfaya geldiğimizde bir Kisiler nesnesi yollamamız gerekiyor.
+  Kisiler kisi;
+  DetaySayfa({required this.kisi});
 
   @override
   State<DetaySayfa> createState() => _DetaySayfaState();
 }
 
+//kişi güncelleme işleminin modüler olması için ayrı bir fonksiyonda bu işlemi yapıyoruz.
+Future<void> kisiGuncelle(int kisi_id, String kisi_ad, String kisi_tel) async{
+  print("Kişi Güncelle: $kisi_id - $kisi_ad - $kisi_tel");
+}
+
 class _DetaySayfaState extends State<DetaySayfa> {
+  // text fieldlar için controller oluştur.
+  var tfKisiAdController = TextEditingController();
+  var tfKisiTelController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
     appBar: AppBar(title: const Text("Kişi Detay"),),
-    body: const Center(),
+    body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 50, right: 50),
+          child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextField(controller: tfKisiAdController, decoration: const InputDecoration(hintText: "Kişi Ad"),),
+              TextField(controller: tfKisiTelController, decoration: const InputDecoration(hintText: "Kişi Tel"),),
+              ElevatedButton(onPressed: (){
+                // veri kaydetme işlemi
+              }, child: const Text("KAYDET")),
+            ],
+          ),
+        )
+    ),,
     );
   }
 }
