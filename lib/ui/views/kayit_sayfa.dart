1cb1286c78cbc51065a93a2163f1e2dfc4090e0d
oznur.demir <dmroznurr@gmail.com>
2024-08-23 18:05:45 +0300

    Completed kayit_sayfa.dart UI.

--- lib/ui/views/kayit_sayfa.dart
+++ lib/ui/views/kayit_sayfa.dart
@@ -7,12 +7,35 @@ class KayitSayfa extends StatefulWidget {
   State<KayitSayfa> createState() => _KayitSayfaState();
 }
 
+//kişi kayıt işleminin modüler olması için ayrı bir fonksiyonda bu işlemi yapıyoruz.
+Future<void> kisiKaydet(String kisi_ad, String kisi_tel) async{
+  print("Kişi kayıt: $kisi_ad - $kisi_tel");
+}
+
 class _KayitSayfaState extends State<KayitSayfa> {
+  // text fieldlar için controller oluştur.
+  var tfKisiAdController = TextEditingController();
+  var tfKisiTelController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Kişi Kayıt"),),
-      body: const Center(),
+      // widgetları yatayda ortalamak istediğimiz içn Cneter() yapısını kullanıyoruz.
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 50, right: 50),
+          child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextField(controller: tfKisiAdController, decoration: const InputDecoration(hintText: "Kişi Ad"),),
+              TextField(controller: tfKisiTelController, decoration: const InputDecoration(hintText: "Kişi Tel"),),
+              ElevatedButton(onPressed: (){
+                // veri kaydetme işlemi
+                kisiKaydet(tfKisiAdController.text, tfKisiTelController.text);
+              }, child: const Text("KAYDET")),
+            ],
+          ),
+        )
+      ),
     );
   }
 }
