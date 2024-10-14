2d99433de0255a72f039ff9ec3823914d1397257
oznur.demir <oznur.demir@beymen.com>
2024-10-14 15:09:44 +0300

    Completed kisiler_cevap.dart and kisiler.dart

--- lib/data/entity/kisiler.dart
+++ lib/data/entity/kisiler.dart
@@ -5,4 +5,13 @@ class Kisiler{
   String kisi_tel;
 
   Kisiler({required this.kisi_id, required this.kisi_ad, required this.kisi_tel});
+
+  //Veri gelirse bu veriyi doğru isimlerle doğru veri tipine dönüştür.
+  factory Kisiler.fromJson(Map<String, dynamic> json) {
+    return Kisiler(
+        kisi_id: json["kisi_id"] as String,
+        kisi_ad: json["kisi_ad"] as String,
+        kisi_tel: json["kisi_tel"] as String
+    );
+  }
 }
--- lib/data/entity/kisiler_cevap.dart
+++ lib/data/entity/kisiler_cevap.dart
@@ -5,4 +5,14 @@ class KisilerCevap {
   int success;
 
   KisilerCevap({required this.kisiler, required this.success});
+
+  factory KisilerCevap.fromJson(Map<String, dynamic> json) {
+        var jsonArray = json["kisiler"] as List;
+        int success = json["success"] as int;
+
+        // bu sırasıyla tüm json nesnelerini alıp kisiler sınıfına atacak. Orada da json nesnesi, kişi nesnesine çevrilecek.
+        var kisiler = jsonArray.map((jsonArrayNesnesi) => Kisiler.fromJson(jsonArrayNesnesi)).toList();
+
+        return KisilerCevap(kisiler: kisiler, success: success);
+  }
 }
